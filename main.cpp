8c0f41f2b6bb237fa1e52d7e1fe620d8de903fab
yuvio100 <yuvio100@csu.fullerton.edu>
2020-05-18 01:12:21 -0700

    almost done

--- main.cpp
+++ main.cpp
@@ -4,20 +4,38 @@ Yashab Narang and Kyle Ear
 */
 
 #include <iostream>
-#include <stdlib.h>
-#include <unistd.h>
-#include <signal.h>
-#include <iomanip>
-#include <stdio.h>
 #include <fstream>
-#include <vector>
+#include <iomanip>
 #include <queue>
 
 using namespace std;
 
-#include "data.h"
+struct process
+{
+  int pid;
+  int arrivalTime, lifeTime;
+  int memPieces, memReq;
+};
 
-void printMemoryMap(vector<memPage>MemMap, int PageTotal)
+struct memoryPage
+{
+	int pageStart, pageEnd, _pid, pageNum, arrivalTime, endTime;
+};
+
+void getInputQueue(queue<process> _inputQueue)
+{
+  cout << "\tInput Queue:[";
+  while (!_inputQueue.empty())
+  {
+    cout << _inputQueue.front().pid;
+    _inputQueue.pop();
+    if(!_inputQueue.empty())
+      cout << " ";
+  }
+  cout << "]" << endl;
+}
+
+void printMemoryMap(vector<memoryPage>MemMap, int PageTotal)
 {
     cout << "\tMemory Map:" << endl;
 
@@ -25,13 +43,11 @@ void printMemoryMap(vector<memPage>MemMap, int PageTotal)
     int End = 0;
 
     for (int x = 0; x < PageTotal; x++)
-    {    memPage N = MemMap [x];
+    {
+      memoryPage N = MemMap [x];
 
    	 if (N._pid == -1)
-   	 {
    	   Start = N.pageStart;
-   	 }
-
    	 End = N.pageEnd;
 
    	 if ( x == PageTotal -1)
@@ -42,119 +58,109 @@ void printMemoryMap(vector<memPage>MemMap, int PageTotal)
     else
     {
    	 if (End != 0)
-   	 { cout << "\t\t" << Start << "-" << End << ":";
+   	 {
+       cout << "\t\t" << Start << "-" << End << ":";
 
-   	 cout << "\t Free Frame(s) \n";
-
-   	 cout<< "\t\t" << N.pageStart << "-" << N.pageEnd << ":" ;
-   	 cout<<"\tProcess" << N._pid << "\tPage" << N.pageNum << endl ;
-
+       cout << "\t Free Frame(s) \n";
+       cout<< "\t\t" << N.pageStart << "-" << N.pageEnd << ":";
+       cout<<"\tProcess" << N._pid << "\tPage" << N.pageNum << endl;
    	 }
 
-   	 else {
-   		 cout << "\t\t" << N.pageStart << "-" << N.pageEnd << ":" ;
-
+   	 else
+     {
+   		 cout << "\t\t" << N.pageStart << "-" << N.pageEnd << ":";
    		 cout << "\tProcess" << N._pid << "\tPage" << N.pageNum << endl;
-
-
-   		 }
-    }
-    }
+     }
+   }
+ }
 }
 
-int main() {
+int main()
+{
 
-  long int memSize = 0;
+  long int memorySize = 0;
   long int pageSize = 0;
-  char filename;
-  size_t result;
+
   long int virtualClock = 0;
-  int numProcess;
+  int numberProcesses = 0;
+  int freePages = 0;
+  char file;
+  size_t result;
 
   queue<process> inputQueue, waitQueue;
-  process tempProcess;
+  process tmp;
 
-  fprintf(stderr, "Please enter in a memory size (must be <= 30,000): ");
-  scanf("%ld", &memSize);
+  //User Input
+  cout << "Enter Memory Size <= 30,000: ";
+  cin >> memorySize;
 
-  if(memSize > 30000) {
-      fprintf(stderr, "The memory size is greater than the maximum size (30,000): %ld\n", memSize);
-      exit(-1);
+  while(memorySize > 30000)
+  {
+    cout << "Memory Too Much. Please Enter Memory Size <= 30,000: ";
+    cin >> memorySize;
   }
 
-  fprintf(stderr, "Please enter in a page size (1: 100, 2: 200, 3: 400): ");
-  scanf("%ld", &pageSize);
+  cout << "Enter Page Size (1: 100, 2: 200, 3: 400): ";
+  cin >> pageSize;
 
-  if(pageSize != 1 && pageSize != 2 && pageSize != 3 ) {
-      fprintf(stderr, "The page size is invalid: %ld\n", pageSize);
-      exit(-1);
-  }
-  switch (pageSize) {
-    case 1:
-      pageSize = 100;
-      break;
-    case 2:
-      pageSize = 200;
-      break;
-    case 3:
-      pageSize = 400;
-      break;
+  while(pageSize < 1 || pageSize > 3)
+  {
+    cout << "Invalid Page Size. Enter Page Size (1: 100, 2: 200, 3: 400): ";
+    cin >> pageSize;
   }
 
-  int freePages = memSize / pageSize;
-  fprintf(stderr, "free pages = %d\n", freePages);
+  if(pageSize == 1)
+    pageSize = 100;
+  if(pageSize == 2)
+    pageSize = 200;
+  if(pageSize == 3)
+    pageSize = 400;
 
-  fprintf(stderr, "Please enter the name of the workload file: ");
-  scanf("%s", &filename);
+  freePages = memorySize / pageSize;
 
-  ifstream inputFile;
-  inputFile.open(&filename);
-  if (!inputFile) {
-    perror("Error reading file.");
-    exit(-1);
+  cout << "Enter Workload File Name: ";
+  cin >> &file;
+
+  ifstream inFile;
+  inFile.open(&file);
+
+  while(!inFile)
+  {
+    cout << "Invalid File Name. Enter Valid File Name: ";
+    cin >> &file;
+    inFile.open(&file);
   }
 
-  inputFile >> numProcess;
+  inFile >> numberProcesses;
 
-  for (int i = 0; i < numProcess; i++) {
+  for (int i = 0; i < numberProcesses; i++)
+  {
+    process tmp;
 
-    process tempProcess;
-
-    inputFile >> tempProcess.pid;
-    if (inputFile.fail()) {
-      perror("Error reading process id (line 1).");
-      exit(-1);
-    }
-
-    inputFile >> tempProcess.arrivalTime >> tempProcess.lifeTime;
-    if (inputFile.fail()) {
-      perror("Error reading times (line 2).");
-      exit(-1);
-    }
-
-    inputFile >> tempProcess.memPieces;
-    if (inputFile.fail()) {
-      perror("Error reading address space (line 3).");
-      exit(-1);
-    }
+    inFile >> tmp.pid;
+    inFile >> tmp.arrivalTime >> tmp.lifeTime;
+    inFile >> tmp.memPieces;
 
     int total = 0;
     int temp = 0;
-    for (int j = 0; j < tempProcess.memPieces; j++) {
-      inputFile >> temp;
+    for (int j = 0; j < tmp.memPieces; j++)
+    {
+      inFile >> temp;
       total += temp;
     }
-    tempProcess.memReq = total;
+    tmp.memReq = total;
 
-    waitQueue.push(tempProcess);
+    waitQueue.push(tmp);
   }
 
-    memPage begin;
-    vector<memPage> memoryMap;
+    memoryPage begin;
+    vector<memoryPage> memoryMap;
     vector<int> turnAroundTimes;
-    int numOfPages = memSize/pageSize;
+    int numOfPages = memorySize / pageSize;
     int finProcID = waitQueue.back().pid;
-    for (int k = 0; k < numOfPages; k++){
+
+    for (int k = 0; k < numOfPages; k++)
+    {
       begin.pageStart = k * pageSize;
       begin.pageEnd = ((k + 1) * pageSize) - 1;
       begin.pageNum = 0;
@@ -165,46 +171,37 @@ int main() {
 
     int timeLimit = 100000;
 
-    while (virtualClock != timeLimit) {
+    while (virtualClock != timeLimit)
+    {
       bool print = true;
       bool waitTest = false;
       bool printMap = false;
       process frontP = waitQueue.front();
 
       if (!waitQueue.empty())
-      {
         waitTest = (frontP.arrivalTime == virtualClock);
-      }
 
       while(waitQueue.front().arrivalTime == virtualClock)
       {
-        cout << "t= " << virtualClock << endl;
-        cout << "Process " << waitQueue.front().pid << " arrives" << endl;
+        cout << "t = " << virtualClock << ": Process " << waitQueue.front().pid << " arrives" << endl;
         inputQueue.push(waitQueue.front());
         waitQueue.pop();
-
-        cout << "Input Queue:[";
-        while (!_inputQueue.empty()) {
-          cout << _inputQueue.front().pid << " ";
-          _inputQueue.pop();
-        }
-        cout << "]" << endl;
-
+        getInputQueue(inputQueue);
         printMap = true;
         print = false;
-        if (!waitQueue.empty()) {
+        if (!waitQueue.empty())
           waitTest = (frontP.arrivalTime == virtualClock);
-        }
-        else {
+        else
           waitTest = false;
-        }
       }
       bool pprint = true;
 
-    for (int i = 0; i < numOfPages; i++){
+    for (int i = 0; i < numOfPages; i++)
+    {
         int procDone = 0;
 
-        if(memoryMap[i].endTime == virtualClock) {
+        if(memoryMap[i].endTime == virtualClock)
+        {
             procDone = memoryMap[i]._pid;
             memoryMap[i]._pid = -1;
             memoryMap[i].pageNum = 0;
@@ -212,38 +209,43 @@ int main() {
             freePages++;
         }
 
-        if ((procDone != 0) && (pprint)) {
-            if (print) {
+        if ((procDone != 0) && (pprint))
+        {
+            if (print)
+            {
               cout << "virtual clock = " << virtualClock << endl;
               print = false;
             }
+
             cout << "\t\tProcess " << procDone << " completes\n";
             printMap = true;
             pprint = false;
         }
-
     }
 
-      while ((!inputQueue.empty()) && (freePages > 0)) {
-        if (print) {
+      while ((!inputQueue.empty()) && (freePages > 0))
+      {
+        if (print)
+        {
           cout << "virtual clock = " << virtualClock << endl;
           print = false;
         }
-        cout << "\t\tMM moves Process " << inputQueue.front().pid << " to memory\n";
+        cout << "\tMM moves Process " << inputQueue.front().pid << " to memory\n";
         printMap = true;
         int endT = virtualClock + inputQueue.front().lifeTime;
         int pNum = 1;
         int i = 0;
 
-        if (memoryMap[i]._pid == finProcID) {
+        if (memoryMap[i]._pid == finProcID)
           timeLimit = endT;
-        }
 
         memoryMap[i].arrivalTime = inputQueue.front().arrivalTime;
         turnAroundTimes.push_back(memoryMap[i].endTime - memoryMap[i].arrivalTime);
 
-        while ((inputQueue.front().memReq != 0) && (i < numOfPages)) {
-          if (memoryMap[i]._pid == -1) {
+        while ((inputQueue.front().memReq != 0) && (i < numOfPages))
+        {
+          if (memoryMap[i]._pid == -1)
+          {
             memoryMap[i]._pid = inputQueue.front().pid;
             memoryMap[i].pageNum = pNum;
             memoryMap[i].endTime = endT;
@@ -256,20 +258,18 @@ int main() {
         inputQueue.pop();
       }
 
-    if(printMap) {
+    if(printMap)
       printMemoryMap(memoryMap, numOfPages);
-    }
-    virtualClock++;
 
+    virtualClock++;
   }
 
   int total = 0;
-  for (int i = 0; i < turnAroundTimes.size(); i++) {
+  for (int i = 0; i < turnAroundTimes.size(); i++)
     total += turnAroundTimes[i];
-  }
+
   double turnAround = total / turnAroundTimes.size();
   cout << "\nAverage Turnaround Time: " << fixed << setprecision(2) << turnAround << "\n" << endl;
 
   return 0;
-
 }
